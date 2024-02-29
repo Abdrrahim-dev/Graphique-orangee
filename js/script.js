598c9e2b8c4862622a8cba8efd9b44f827c730c7
Abderrahim <onlymahami@gmail.com>
2024-02-29 15:28:28 +0100

    finish the contact and the responsive issues

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ links.forEach((link) => {
 });
 
 //
+let bars = document.querySelector(".bars");
+let home = document.querySelector(".home");
+bars.addEventListener("click", function () {
+  header.classList.toggle("phone");
+  let headerPhone = document.querySelector("header.phone");
+  headerPhone.style.display = "flex";
+  home.style.visibility = "hidden";
+});
+//
+let exit = header.querySelector(".exit");
+exit.addEventListener("click", function () {
+  header.classList.remove("phone");
+  header.style.display = "none";
+  home.style.visibility = "visible";
+});
